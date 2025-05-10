9d4d2ae4ae2ea055ea145b439d891182204af706
Gustavohlma <gustavohlma8@gmail.com>
2025-05-09 23:59:42 -0300

    fix(main): aumenta limite nas requisições express para comportar imagens

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(router);
 
 export default app;
--- src/routes/VisionRoutes.ts
+++ src/routes/VisionRoutes.ts
@@ -5,5 +5,8 @@ const visionRoutes = Router();
 const visionController = new VisionController();
 
 visionRoutes.post('/analyze-image', visionController.analyzeImage);
+visionRoutes.get('/', (req, res) => {
+  res.status(200).json({ message: 'Rota de teste para análise de imagem' });
+});
 
 export default visionRoutes;
